d3e62b958afb0c6cb9430ac7ce146e491fa3d3e2
Thibaut Broggi <broggi_t@epitech.eu>
2017-01-24 13:33:31 +0000

    Add env var PAGER

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,7 @@ end
 
 set -x TERM xterm-256color
 set -x EDITOR 'emacs -nw'
+set -x PAGER less
 set -x MANPAGER most
 set -gx PATH $PATH ~/.gem/ruby/2.3.0/bin ~/.rbenv/bin ~/.nodenv/bin
 set -x MYSQL_PS1 (echo -e "\e[1;33m\u\e[0m@\e[1;31mMariaDB\e[0m:\e[1;32m\d\e[0m") '\n    -> '
